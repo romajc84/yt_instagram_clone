e9a57cfdfb3c2de5f01c08b96f7b01096f2df8d4
romajc84 <joeyroman84@gmail.com>
2021-06-23 18:25:40 -0400

    first commit

---
+++ lib/pages/insta_home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class InstaHome extends StatelessWidget {
+  final topBar = AppBar(
+    backgroundColor: Color(0xfff8faf8),
+    centerTitle: true,
+    elevation: 1.0,
+    leading: Icon(Icons.camera_alt),
+    title: SizedBox(
+      height: 35.0,
+      child: Image.asset('assets/images/insta_logo.png'),
+    ),
+    actions: [
+      Padding(
+        padding: EdgeInsets.only(right: 12.0),
+        child: Icon(Icons.send),
+      )
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: topBar,
+      body: Container(),
+    );
+  }
+}
