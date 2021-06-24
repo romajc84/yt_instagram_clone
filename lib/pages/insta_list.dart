f79546f01fd972f84fb35ffbae65fb70b0d13fac
romajc84 <joeyroman84@gmail.com>
2021-06-24 12:49:04 -0400

    finished

--- lib/pages/insta_list.dart
+++ lib/pages/insta_list.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:yt_instagram_clone/pages/isnta_stories.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class InstaList extends StatelessWidget {
+class InstaList extends StatefulWidget {
+  @override
+  _InstaListState createState() => _InstaListState();
+}
+
+class _InstaListState extends State<InstaList> {
+  bool isPressed = false;
+
   @override
   Widget build(BuildContext context) {
     var deviceSize = MediaQuery.of(context).size;
@@ -12,7 +20,137 @@ class InstaList extends StatelessWidget {
               child: InstaStories(),
               height: deviceSize.height * 0.15,
             )
-          : Column(),
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      // 1st row
+                      Row(
+                        children: <Widget>[
+                          new Container(
+                            height: 40.0,
+                            width: 40.0,
+                            decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: new NetworkImage(
+                                      "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+                            ),
+                          ),
+                          new SizedBox(
+                            width: 10.0,
+                          ),
+                          new Text(
+                            "imthpk",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      new IconButton(
+                        icon: Icon(Icons.more_vert),
+                        onPressed: null,
+                      )
+                    ],
+                  ),
+                ),
+                // 2nd row
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: new Image.network(
+                    "https://images.pexels.com/photos/672657/pexels-photo-672657.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                // 3rd row
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(isPressed
+                                ? Icons.favorite
+                                : FontAwesomeIcons.heart),
+                            color: isPressed ? Colors.red : Colors.black,
+                            onPressed: () {
+                              setState(() {
+                                isPressed = !isPressed;
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 16.0,
+                          ),
+                          Icon(
+                            FontAwesomeIcons.comment,
+                          ),
+                          SizedBox(
+                            width: 16.0,
+                          ),
+                          Icon(FontAwesomeIcons.paperPlane),
+                        ],
+                      ),
+                      new Icon(FontAwesomeIcons.bookmark)
+                    ],
+                  ),
+                ),
+                // 4th row
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Liked by pawankumar, pk and 528,331 others",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                //5th row
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        height: 40.0,
+                        width: 40.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: NetworkImage(
+                                  "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Add a comment...",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                //6th row
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child:
+                      Text("1 Day Ago", style: TextStyle(color: Colors.grey)),
+                )
+              ],
+            ),
     );
   }
 }
